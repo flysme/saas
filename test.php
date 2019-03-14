af6d7c7730e1cf16ed469f86bbe802c385f7fe15
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-14 13:54:07 +0800

    first2

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?php
-echo 11111111;
-// $db_host = "127.0.0.1";//localhost
-// $db_user = "root";//用户名
-// $db_pwd = "D8B7d904e33e";//密码
-// $db_name = "saas";//数据库名
-// //链接数据库代码
-// $this->links = new mysqli($db_host,$db_user,$db_pwd,$db_name);
-// !mysqli_connect_error() or die("连接失败！！");
-// $res = $this->links->query("SELECT * FROM product");
-// $arr = mysqli_fetch_assoc($res);
-// var_dump($arr);
+// echo 11111111;
+$db_host = "127.0.0.1";//localhost
+$db_user = "root";//用户名
+$db_pwd = "D8B7d904e33e";//密码
+$db_name = "saas";//数据库名
+//链接数据库代码
+$links = new mysqli($db_host,$db_user,$db_pwd,$db_name);
+!mysqli_connect_error() or die("连接失败！！");
+$res = $links->query("SELECT * FROM product");
+$arr = mysqli_fetch_assoc($res);
+var_dump($arr);
  ?>
